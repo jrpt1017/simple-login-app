f207946852176d8e54c21196ba46108fcaf7085b
jrpt1017 <jonneltolosa111@gmail.com>
2022-07-16 15:25:17 +0800

    Initial setup of store for RTK

---
+++ redux/slices/index.js
@@ -0,0 +1,48 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  isLoggedIn: false,
+  user: {
+    id: "",
+    name: "",
+    username: "",
+    email: "",
+    address: {
+      street: "",
+      suite: "",
+      city: "",
+      zipcode: "",
+      geo: {
+        lat: "",
+        lng: "",
+      },
+    },
+    phone: "",
+    website: "",
+    company: {
+      name: "",
+      catchPhrase: "",
+      bs: "",
+    },
+  },
+};
+
+const userSlice = createSlice({
+  name: "userState",
+  initialState,
+  reducers: {
+    logInUser: (state, action) => {
+      (state.isLoggedIn = true), (state.user = action.payload);
+    },
+    logOutUser: () => {
+      (state.isLoggedIn = false), (state.user = {});
+    },
+  },
+});
+
+export const { logInUser, logOutUser } = userSlice.actions;
+export default userSlice.reducer;
+
+export const userDetails = (state) => {
+  return state.userState.user;
+};
